1adfc26e13a50292ac813483be039c7eaa5de9b1
Roman Sokolov <sokoloff.rv@gmail.com>
2025-11-25 00:35:07 +0600

    Установлен Yii2 и настроен на работу с phpdotenv

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->safeLoad();
+
+defined('YII_DEBUG') or define('YII_DEBUG', ($_ENV['YII_DEBUG'] ?? 'false') === 'true');
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV'] ?? 'prod');
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+$config = require __DIR__ . '/../config/web.php';
+
+(new yii\web\Application($config))->run();
